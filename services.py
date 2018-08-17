445b459aab3092c825582ae1d386423813a472f0
Richard Barrios <rhrichardbm@gmail.com>
2018-08-17 17:58:25 -0300

    feat(services):  add services to verify permissions

---
+++ services.py
@@ -0,0 +1,47 @@
+# Use modern Python
+from __future__ import unicode_literals, absolute_import, print_function
+
+from django.db.models import Q
+
+from apps.user_roles.models import Permission, UserGrant, GroupGrant
+
+
+def has_permission(user_id, action_name, **parameter_values):
+    """
+    Verify if one user have permissions to perform the requested action with the given parameters
+    :param user_id: User id
+    :param action_name: string Permission name
+    :param parameter_values: the grant parameter values
+    :return: bool
+    """
+    permission = Permission.objects.get(code=action_name)
+
+    if has_user_grant(user_id, permission, parameter_values):
+        return True
+
+    if has_group_grant(user_id, permission, parameter_values):
+        return True
+
+    return False
+
+
+def has_user_grant(user_id, permission, parameter_values):
+    """
+    Verify if one user have the given permission through UserGrant
+    """
+    return permission.permission_user_grants.filter(
+        user_id=user_id
+    ).filter(
+        Q(parameter_values=parameter_values) | Q(parameter_values={})
+    ).exists()
+
+
+def has_group_grant(user_id, permission, parameter_values):
+    """
+    Verify if one user have the given permission through GroupGrant
+    """
+    return permission.permission_group_grants.filter(
+        group__user__id=user_id
+    ).filter(
+        Q(parameter_values=parameter_values) | Q(parameter_values={})
+    ).exists()
