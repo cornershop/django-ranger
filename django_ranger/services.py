cced65bb639315581a5f5489a26c59ec225daabe
Richard Barrios <rhrichardbm@gmail.com>
2018-08-20 20:34:04 -0300

    chore(services): comments on PermissionManager

--- django_ranger/services.py
+++ django_ranger/services.py
@@ -9,6 +9,12 @@ from .models import Permission, GroupGrant
 
 
 class PermissionManager(object):
+    """
+    This class allows managing user permissions.
+
+    It is instantiated with a user instance and enlists all their permissions
+     grants to achieve a better performance in multiple permission verifications.
+    """
 
     def __init__(self, user):
         self.data = []
@@ -21,18 +27,28 @@ class PermissionManager(object):
         self.data = list(set(self.data))
 
     def set_grant_hashes(self, grants):
+        """
+        store the given grants into a hashes list.
+        """
         for grant in grants:
             hash_code = self.get_hash(grant.permission.code, grant.parameter_values)
             self.data.append(hash_code)
 
     @staticmethod
     def get_hash(permission_code, parameter_values):
+        """
+        It's return a hash of the permission and their parameters.
+        """
         parameters_string = json.dumps(parameter_values, sort_keys=True)
         string_code = "{}-{}".format(permission_code, parameters_string)
         hash_code = hashlib.sha256(string_code)
         return str(hash_code)
 
     def has_permission(self, action_name, **parameter_values):
+        """
+        Verifies if the instantiated user has the given permission with
+        the given parameters.
+        """
         hash_code = self.get_hash(action_name, parameter_values)
         return hash_code in self.data
 
