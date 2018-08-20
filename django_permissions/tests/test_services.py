7979b9a97573c407cd6b9331fb2a5db0312d8c54
Richard Barrios <rhrichardbm@gmail.com>
2018-08-20 12:14:14 -0300

    chore(tests): add test to has_permission services

--- django_permissions/tests/test_services.py
+++ django_permissions/tests/test_services.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
 from django.conf import settings
+from django.test import TestCase
 from model_mommy import mommy
 
+from ..models import Permission
 from ..services import has_permission
 
 
@@ -9,10 +10,75 @@ class HasPermissionTestCase(TestCase):
 
     def setUp(self):
         self.user = mommy.make(settings.AUTH_USER_MODEL)
+        self.group = mommy.make("auth.Group")
+        self.user.groups.add(self.group)
         self.can_view_code = "can_view:module"
+        self.can_view_with_param_code = "can_view_with_param:module"
         self.can_view_permission = mommy.make("django_permissions.Permission", code=self.can_view_code)
+        self.can_view_permission_with_param = mommy.make("django_permissions.Permission",
+                                                         code=self.can_view_with_param_code,
+                                                         parameters_definition=["model_id"])
 
     def test_user_has_permission(self):
         mommy.make("django_permissions.UserGrant", user=self.user, permission=self.can_view_permission)
         response = has_permission(self.user.id, self.can_view_permission)
         self.assertTrue(response)
+
+    def test_user_has_permission_with_params(self):
+        params = {
+            "model_id": 1
+        }
+        mommy.make("django_permissions.UserGrant", user=self.user,
+                   permission=self.can_view_permission_with_param,
+                   parameter_values=params)
+        response = has_permission(self.user.id, self.can_view_with_param_code, model_id=1)
+        self.assertTrue(response)
+
+    def test_user_has_not_permission_with_params(self):
+        params = {
+            "model_id": 1
+        }
+        mommy.make("django_permissions.UserGrant", user=self.user,
+                   permission=self.can_view_permission_with_param,
+                   parameter_values=params)
+        response = has_permission(self.user.id, self.can_view_with_param_code)
+        self.assertFalse(response)
+
+    def test_user_has_not_permission(self):
+        response = has_permission(self.user.id, self.can_view_permission)
+        self.assertFalse(response)
+
+    def test_user_group_has_permission(self):
+        mommy.make("django_permissions.GroupGrant", group=self.group, permission=self.can_view_permission)
+        response = has_permission(self.user.id, self.can_view_permission)
+        self.assertTrue(response)
+
+    def test_user_group_has_permission_with_params(self):
+        params = {
+            "model_id": 1
+        }
+        mommy.make("django_permissions.GroupGrant", group=self.group,
+                   permission=self.can_view_permission_with_param,
+                   parameter_values=params)
+        response = has_permission(self.user.id, self.can_view_with_param_code, model_id=1)
+        self.assertTrue(response)
+
+    def test_user_group_has_not_permission_with_params(self):
+        params = {
+            "model_id": 1
+        }
+        mommy.make("django_permissions.GroupGrant", group=self.group,
+                   permission=self.can_view_permission_with_param,
+                   parameter_values=params)
+        response = has_permission(self.user.id, self.can_view_with_param_code)
+        self.assertFalse(response)
+
+    def test_user_group_has_not_permission(self):
+        response = has_permission(self.user.id, self.can_view_permission)
+        self.assertFalse(response)
+
+    def test_permission_does_not_exist(self):
+        with self.assertRaises(Permission.DoesNotExist):
+            has_permission(self.user.id, "has_unexpected_permission:module")
+
+
