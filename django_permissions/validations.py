d9a82f7ff9dff7a1c4e932086b6c68211d5b5ea1
Richard Barrios <rhrichardbm@gmail.com>
2018-08-20 17:10:41 -0300

    fix(validations): validate if grant.parameter_values is a empty list

--- django_permissions/validations.py
+++ django_permissions/validations.py
@@ -5,7 +5,7 @@ class ValidatingGrantModel(object):
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         definition = self.permission.parameters_definition
         values = self.parameter_values.keys()
-        if definition != values:
+        if definition != values and values != []:
             msg = u"parameter_values content is inconsistent with permission.parameters_definition {}-{}".format(
                 definition, values)
             raise ParameterError(msg)
