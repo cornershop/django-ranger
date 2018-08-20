78eb24e9434c81c595297dbb5366c3b1f2b1f71c
Richard Barrios <rhrichardbm@gmail.com>
2018-08-20 16:09:39 -0300

    refactor(validations): move Validation grant models

---
+++ django_permissions/validations.py
@@ -0,0 +1,12 @@
+from django_permissions.django_permissions.exceptions import ParameterError
+
+
+class ValidatingGrantModel(object):
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        definition = self.permission.parameters_definition
+        values = self.parameter_values.keys()
+        if definition != values:
+            msg = u"parameter_values content is inconsistent with permission.parameters_definition {}-{}".format(
+                definition, values)
+            raise ParameterError(msg)
+        super(ValidatingGrantModel, self).save(force_insert, force_update, using, update_fields)
