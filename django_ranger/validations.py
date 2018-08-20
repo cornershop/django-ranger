9857727aae55c303d917d8061cf377a379595141
Richard Barrios <rhrichardbm@gmail.com>
2018-08-20 20:34:25 -0300

    chore(validations): comments on ValidatingGrantModel Mixin

--- django_ranger/validations.py
+++ django_ranger/validations.py
@@ -2,6 +2,13 @@ from .exceptions import ParameterError
 
 
 class ValidatingGrantModel(object):
+    """
+    A validation Mixin for  UserGrant and GroupGrant models.
+
+    it verifies that the `parameter_values` of the grant instance be consistent
+    with the parameters defined in the permission instance.
+
+    """
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         definition = self.permission.parameters_definition
         values = self.parameter_values.keys()
