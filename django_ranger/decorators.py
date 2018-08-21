263f0f4511e622037b00d652e29e6da754ce741a
Richard Barrios <rhrichardbm@gmail.com>
2018-08-21 20:41:24 -0300

    feat(decorators): add decorator for permission verification for api view

--- django_ranger/decorators.py
+++ django_ranger/decorators.py
@@ -1,6 +1,8 @@
 from functools import wraps
 
 from django.http import HttpResponseRedirect
+from rest_framework import status
+from rest_framework.response import Response
 
 from .services import PermissionManager
 
@@ -33,6 +35,37 @@ def permission_required(action_list=None, *args, **kwargs):
             if not user_permission.has_any_permission(action_list):
                 # TODO change URL for a url set in the django settings
                 return HttpResponseRedirect("/accounts/login/?next=" + request.path)
+
+            return function(obj, *args, **kwargs)
+        return wrapper
+    return renderer
+
+
+def api_permission_required(action_list=None, *args, **kwargs):
+    """
+    This decorator verifies if the user has permissions to perform
+    any action contained by `action_list`.
+
+    The `action_list` param expect the structure described bellow:
+
+    [('can_view:module', {'module_id': 1}), ('can_manage:module', {'module_id': 12})]
+
+    This decorator only works over api views.
+    """
+    def renderer(function):
+        @wraps(function)
+        def wrapper(obj, *args, **kwargs):
+            if not hasattr(obj, "user"):
+                raise ValueError("ERROR: The specified object is not a proper request")
+
+            user = obj.user
+
+            if not user.is_authenticated():
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+            user_permission = PermissionManager(user)
+            if not user_permission.has_any_permission(action_list):
+                return Response(status=status.HTTP_403_FORBIDDEN)
 
             return function(obj, *args, **kwargs)
         return wrapper
