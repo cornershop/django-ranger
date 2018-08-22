d203ca02c29555b65eab0885f7104a2e2ba77bc5
Richard Barrios <rhrichardbm@gmail.com>
2018-08-22 18:13:51 -0300

    chore(services): remove has_permission method and dependeces

--- django_ranger/tests/test_services.py
+++ django_ranger/tests/test_services.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.test import TestCase
 from model_mommy import mommy
 
-from ..models import Permission
-from ..services import has_permission, has_group_grant, has_user_grant, PermissionManager
+from ..services import PermissionManager
 
 
 class HasPermissionTestCase(TestCase):
@@ -18,77 +17,6 @@ class HasPermissionTestCase(TestCase):
         self.can_view_permission_with_param = mommy.make("django_ranger.Permission",
                                                          code=self.can_view_with_param_code,
                                                          parameters_definition=["model_id"])
-
-    def test_has_permission(self):
-        mommy.make("django_ranger.UserGrant", user=self.user, permission=self.can_view_permission)
-        response = has_permission(self.user.id, self.can_view_permission)
-        self.assertTrue(response)
-
-    def test_has_group_permission(self):
-        mommy.make("django_ranger.GroupGrant", group=self.group, permission=self.can_view_permission)
-        response = has_permission(self.user.id, self.can_view_permission)
-        self.assertTrue(response)
-
-    def test_has_not_permission(self):
-        response = has_permission(self.user.id, self.can_view_permission)
-        self.assertFalse(response)
-
-    def test_permission_does_not_exist(self):
-        with self.assertRaises(Permission.DoesNotExist):
-            has_permission(self.user.id, "has_unexpected_permission:module")
-
-    def test_user_has_permission_with_params(self):
-        params = {
-            "model_id": 1
-        }
-        mommy.make("django_ranger.UserGrant", user=self.user,
-                   permission=self.can_view_permission_with_param,
-                   parameter_values=params)
-        response = has_user_grant(self.user.id, self.can_view_permission_with_param, params)
-        self.assertTrue(response)
-
-    def test_user_has_not_permission_with_params(self):
-        params = {
-            "model_id": 1
-        }
-        mommy.make("django_ranger.UserGrant", user=self.user,
-                   permission=self.can_view_permission_with_param,
-                   parameter_values=params)
-        response = has_user_grant(self.user.id, self.can_view_permission_with_param)
-        self.assertFalse(response)
-
-    def test_user_has_not_permission(self):
-        response = has_user_grant(self.user.id, self.can_view_permission)
-        self.assertFalse(response)
-
-    def test_user_group_has_permission(self):
-        mommy.make("django_ranger.GroupGrant", group=self.group, permission=self.can_view_permission)
-        response = has_group_grant(self.user.id, self.can_view_permission)
-        self.assertTrue(response)
-
-    def test_user_group_has_permission_with_params(self):
-        params = {
-            "model_id": 1
-        }
-        mommy.make("django_ranger.GroupGrant", group=self.group,
-                   permission=self.can_view_permission_with_param,
-                   parameter_values=params)
-        response = has_group_grant(self.user.id, self.can_view_permission_with_param, params)
-        self.assertTrue(response)
-
-    def test_user_group_has_not_permission_with_params(self):
-        params = {
-            "model_id": 1
-        }
-        mommy.make("django_ranger.GroupGrant", group=self.group,
-                   permission=self.can_view_permission_with_param,
-                   parameter_values=params)
-        response = has_group_grant(self.user.id, self.can_view_permission_with_param)
-        self.assertFalse(response)
-
-    def test_user_group_has_not_permission(self):
-        response = has_group_grant(self.user.id, self.can_view_permission)
-        self.assertFalse(response)
 
     def test_permission_manager_has_permission(self):
         params = {
