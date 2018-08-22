7cf175449a9fa886280accef5ef4e9ca408c56fb
Richard Barrios <rhrichardbm@gmail.com>
2018-08-22 18:35:59 -0300

    chore(validations): validate model fields

--- django_ranger/validations.py
+++ django_ranger/validations.py
@@ -10,6 +10,12 @@ class ValidatingGrantModel(object):
 
     """
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not hasattr(self, "permission"):
+            raise AttributeError(u"The model must have a permission field")
+
+        if not hasattr(self, "parameter_values"):
+            raise AttributeError(u"The model must have a parameter_values field")
+
         definition = self.permission.parameters_definition
         values = self.parameter_values.keys()
         if definition != values and values != []:
