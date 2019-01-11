e9769b9e4e4e59058102d7db02be61ae116339d2
Richard Barrios <rhrichardbm@gmail.com>
2019-01-11 15:44:17 -0300

    chore(setup):  upgrade django_ranger library

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ranger',
-    version='0.3.1',
+    version='0.3.2',
     packages=['django_ranger', 'django_ranger.migrations'],
     include_package_data=True,
     license='BSD License',
